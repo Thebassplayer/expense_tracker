09b37fc1786d31e05ee27c07d0693a017f4bce96
Roy Lopez <windup.dues-0f@icloud.com>
2022-08-19 21:03:37 -0300

    mobile version v2

--- public/script.js
+++ public/script.js
@@ -3,6 +3,8 @@ const balance = document.getElementById('balance'),
   moneyMinus = document.getElementById('money-minus'),
   list = document.getElementById('list'),
   form = document.getElementById('form'),
+  plusBtn = document.getElementById('option-1'),
+  minusBtn = document.getElementById('option-2'),
   detail = document.getElementById('detail'),
   amount = document.getElementById('amount');
 
@@ -13,9 +15,17 @@ const localStorageTransactions = JSON.parse(
 let transactions =
   localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
+let positiveTransaction = true;
+
 //! Add transaction
 function addTransaction(e) {
   e.preventDefault();
+
+  let signedAmount = Math.abs(amount.value);
+
+  if (!positiveTransaction) {
+    signedAmount = -Math.abs(amount.value);
+  }
 
   if (detail.value.trim() === '' || amount.value.trim() === '') {
     alert('Please add detail and amount');
@@ -23,7 +33,7 @@ function addTransaction(e) {
     const newTransaction = {
       id: generateID(),
       detail: detail.value,
-      amount: +amount.value,
+      amount: signedAmount,
     };
 
     transactions.push(newTransaction);
@@ -41,15 +51,16 @@ function generateID() {
 }
 
 //! Add transactions to DOM list BTN
+
 function addTransactionDOM(transaction) {
   //! Get sign
-  const sign = transaction.amount < 0 ? '-' : '+';
+  const sign = transaction.amount > 0 ? '+' : '-';
 
   //! Create li
   const item = document.createElement('li');
 
   //! Add class based on value
-  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+  item.classList.add(transaction.amount > 0 ? 'plus' : 'minus');
 
   item.innerHTML = `${transaction.detail}<span>${sign}$ ${Math.abs(
     transaction.amount
@@ -115,3 +126,15 @@ init();
 //! Event Listeners
 
 form.addEventListener('submit', addTransaction);
+
+plusBtn.addEventListener('click', () => {
+  positiveTransaction = true;
+  amount.classList.remove('negative');
+  amount.classList.add('positive');
+});
+
+minusBtn.addEventListener('click', () => {
+  positiveTransaction = false;
+  amount.classList.remove('positive');
+  amount.classList.add('negative');
+});
